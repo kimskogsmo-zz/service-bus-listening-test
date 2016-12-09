8e67cb06d4f35ff8e99ee3581309a9ff869bcd61
kimskogsmo <kim.skogsmo@gmail.com>
2016-12-09 23:43:09 +0100

    Source

---
+++ ListenMsgServiceTest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.ServiceBus.Messaging;
+
+namespace ListenMsgServiceTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const string connectionString = "Endpoint=sb://kimspace.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=hNXAPUssGofW9TxU9WUEJe975/+Mcwbh+gv9afNdHls=";
+            const string queueName = "testqueue";
+
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+
+            client.OnMessage(message =>
+            {
+                Console.WriteLine(String.Format("Message body: {0}", message.GetBody<String>()));
+                Console.WriteLine(String.Format("Message id: {0}", message.MessageId));
+                Console.WriteLine("");
+            });
+
+            Console.ReadLine();
+        }
+    }
+}
